88385222323dedc6177b01eb6030719698875e5a
Karol Koltun <karolkoltundev@gmail.com>
2020-06-27 14:41:15 +0200

    Make person class final

--- src/main/java/com/sda/examples/Person.java
+++ src/main/java/com/sda/examples/Person.java
@@ -4,9 +4,9 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Person {
-  private String firstName;
-  private String lastName;
-  private LocalDate dateOfBirth;
+  private final String firstName;
+  private final String lastName;
+  private final LocalDate dateOfBirth;
 
   public Person(String firstName, String lastName, LocalDate dateOfBirth) {
     this.firstName = firstName;
