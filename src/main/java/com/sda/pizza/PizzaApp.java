b6ab4c4085e37be2a0cfdc9ceb62536449846188
Karol Koltun <karolkoltundev@gmail.com>
2020-06-27 14:47:17 +0200

    Change pizza factory

--- src/main/java/com/sda/pizza/PizzaApp.java
+++ src/main/java/com/sda/pizza/PizzaApp.java
@@ -1,11 +1,12 @@
 package com.sda.pizza;
 
+import com.sda.pizza.abstr.factory.PizzaFactoryEnum;
 import com.sda.pizza.swtch.factory.Pizza;
 import com.sda.pizza.swtch.factory.PizzaFactorySwitch;
 
 public class PizzaApp {
   public static void main(String[] args) {
-    Pizza pizza = PizzaFactorySwitch.bakePizza(PizzaFactorySwitch.PizzaType.BACON_AND_CHEESE);
+    Pizza pizza = PizzaFactoryEnum.getPizza(PizzaFactoryEnum.PizzaType.BACON_AND_CHEESE);
 
     System.out.println(pizza);
   }
