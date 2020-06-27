4be46cde6ade05953ba2ebe597ea2d786f89086d
Karol Koltun <karolkoltundev@gmail.com>
2020-06-27 10:45:44 +0200

    Add enum abstract methods

--- src/main/java/com/sda/post/office/PackageStatus.java
+++ src/main/java/com/sda/post/office/PackageStatus.java
@@ -1,12 +1,42 @@
 package com.sda.post.office;
 
 enum PackageStatus {
-  SENT("Package has been sent!", 300),
-  TRAVELLING("Package is travelling!", 301),
-  COURIER("Package is handled by the courier!", 302),
-  DELIVERED("Package has been delivered", 200),
-  LOST("Package has been lost", 400),
-  RETURNED("Package has been returned", 303);
+  SENT("Package has been sent!", 300) {
+    @Override
+    public PackageStatus[] getNextStatuses() {
+      return new PackageStatus[] { TRAVELLING, LOST };
+    }
+  },
+  TRAVELLING("Package is travelling!", 301) {
+    @Override
+    public PackageStatus[] getNextStatuses() {
+      return new PackageStatus[] { COURIER, LOST };
+    }
+  },
+  COURIER("Package is handled by the courier!", 302) {
+    @Override
+    public PackageStatus[] getNextStatuses() {
+      return new PackageStatus[] { DELIVERED, LOST, RETURNED };
+    }
+  },
+  DELIVERED("Package has been delivered", 200) {
+    @Override
+    public PackageStatus[] getNextStatuses() {
+      return new PackageStatus[0];
+    }
+  },
+  LOST("Package has been lost", 400) {
+    @Override
+    public PackageStatus[] getNextStatuses() {
+      return new PackageStatus[0];
+    }
+  },
+  RETURNED("Package has been returned", 303) {
+    @Override
+    public PackageStatus[] getNextStatuses() {
+      return new PackageStatus[0];
+    }
+  };
 
   private final String message;
   private final int code;
@@ -23,6 +53,8 @@ enum PackageStatus {
   public int getCode() {
     return code;
   }
+
+  public abstract PackageStatus[] getNextStatuses();
 
   @Override
   public String toString() {
