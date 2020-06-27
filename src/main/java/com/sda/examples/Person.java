1ad14b178bfa1a0ef44cec336bb0eef31e2d7d08
Karol Koltun <karolkoltundev@gmail.com>
2020-06-27 14:59:19 +0200

    Revert "Merge pull request #3 from kkoltun/praca_domowa_2"
    This reverts commit 17b5eb568a6e9e1604ebb29fea10187313ac0882, reversing
    changes made to b6ab4c4085e37be2a0cfdc9ceb62536449846188.

--- src/main/java/com/sda/examples/Person.java
+++ src/main/java/com/sda/examples/Person.java
@@ -4,10 +4,9 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Person {
-  // NOWY KOMENTARZ KTOREGO NIE BYLO WCZESNIEJ
-  private final String firstName;
-  private final String lastName;
-  private final LocalDate dateOfBirth;
+  private String firstName;
+  private String lastName;
+  private LocalDate dateOfBirth;
 
   public Person(String firstName, String lastName, LocalDate dateOfBirth) {
     this.firstName = firstName;
